eba849f1294015b0a27f59d73589837d9c967eab
Oga-Emma <35017091+Oga-Emma@users.noreply.github.com>
2026-04-06 23:13:43 +0200

    Set screen width, prevent screen resizing

---
+++ src/main/java/com/github/ogaemma/vidmate/MainApplication.java
@@ -0,0 +1,31 @@
+package com.github.ogaemma.vidmate;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Vidmate");
+        stage.setScene(scene);
+
+        var screenBounds = Screen.getPrimary().getVisualBounds();
+        // Set stage to 80% of screen width and height
+        var percentage = 1;
+
+        stage.setMinWidth(screenBounds.getWidth() * percentage);
+        stage.setMinHeight(screenBounds.getHeight() * percentage);
+
+        stage.setMaximized(true);
+        stage.setResizable(false);
+
+        stage.show();
+    }
+}
